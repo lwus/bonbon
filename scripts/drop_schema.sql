d1ec184d9f8a0e903d6c8d09cee5e1146d0b68f6
Lawrence Wu <larry.wu@solana.com>
2022-08-09 12:54:50 -0500

    dev: update drop schema

--- scripts/drop_schema.sql
+++ scripts/drop_schema.sql
@@ -1,9 +1,12 @@
-DROP TABLE IF EXISTS collections;
-DROP TABLE IF EXISTS creators;
+DROP TABLE IF EXISTS transfers;
+DROP TABLE IF EXISTS glazings;
 DROP TABLE IF EXISTS bonbons;
-DROP TYPE IF EXISTS limited_edition;
-DROP TYPE IF EXISTS edition_status;
 
 DROP TABLE IF EXISTS account_keys;
 DROP TABLE IF EXISTS partitions ;
 DROP TABLE IF EXISTS transactions ;
+
+DROP TYPE IF EXISTS limited_edition;
+DROP TYPE IF EXISTS edition_status;
+DROP TYPE IF EXISTS token_meta;
+DROP TYPE IF EXISTS creator;
