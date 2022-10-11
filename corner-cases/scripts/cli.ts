6df96b631aee06c7d6ee6c1919143362f901e6c7
jon wong <j@jnwng.com>
2022-10-11 14:52:17 -0400

    Way more cases

--- corner-cases/scripts/cli.ts
+++ corner-cases/scripts/cli.ts
@@ -2,13 +2,13 @@ import fs from "fs";
 import { Keypair, PublicKey } from "@solana/web3.js";
 import { Command } from "commander";
 import { createNfts } from "./create-nfts";
+import { cloneNft } from "./clone-nft";
+import { dustAddress } from "./dust-address";
+import cases from "./cases";
 
 const program = new Command();
 
-const parseDestinationPublicKey = (value) => new PublicKey(value);
-
-const parseKeypair = (value, previousValue) => {
-  console.info({ value });
+const parseKeypair = (value) => {
   const keypairFile = fs.readFileSync(value, "utf-8");
   return Keypair.fromSecretKey(Buffer.from(JSON.parse(keypairFile)));
 };
@@ -32,6 +32,70 @@ program
     await createNfts(destinationAddress, { keypair, url });
   });
 
-program.parse(process.argv);
+program
+  .command("clone <mintAddress> <destination>")
+  .description("Clones an NFT at <mintAddress> into <destination> wallet")
+  .option(
+    "-k, --keypair <path>",
+    "Filepath or URL to a keypair. Only handles fees.",
+    parseKeypair
+  )
+  .option("-u, --url <url>", "RPC URL", "https://devnet.genesysgo.net/")
+  .action(async (mintAddress, destination, { keypair, url }) => {
+    const sourceMintAddress = new PublicKey(mintAddress);
+    const destinationAddress = new PublicKey(destination);
 
-console.info(program.opts());
+    console.info(
+      `Cloning ${sourceMintAddress} into ${destinationAddress.toBase58()} via ${url}`
+    );
+
+    await cloneNft(sourceMintAddress, destinationAddress, { keypair, url });
+  });
+
+program
+  .command("dust <destination> <numberOfTransactions>")
+  .description(
+    "Dusts an account at <destination> with enough SOL for <numberOfTransactions>"
+  )
+  .option(
+    "-k, --keypair <path>",
+    "Filepath or URL to a keypair. Only handles fees.",
+    parseKeypair
+  )
+  .option("-u, --url <url>", "RPC URL", "https://devnet.genesysgo.net/")
+  .action(async (destination, numberOfTransactions, { keypair, url }) => {
+    const destinationAddress = new PublicKey(destination);
+
+    console.info(
+      `Dusting ${destinationAddress.toBase58()} with enough SOL for ${numberOfTransactions} transaction(s) via ${url}`
+    );
+
+    await dustAddress(destinationAddress, numberOfTransactions, {
+      keypair,
+      url,
+    });
+  });
+
+program
+  .command("case <caseId> <destination>")
+  .description("Create a specific case and mint into <destination>")
+  .option(
+    "-k, --keypair <path>",
+    "Filepath or URL to a keypair. Only handles fees.",
+    parseKeypair
+  )
+  .option("-u, --url <url>", "RPC URL", "https://devnet.genesysgo.net/")
+  .action(async (caseId, destination, { keypair, url }) => {
+    const destinationAddress = new PublicKey(destination);
+
+    const foundCase = cases[caseId];
+    if (foundCase) {
+      console.info(
+        `Executing case ${caseId} for address ${destinationAddress.toBase58()}`
+      );
+
+      await foundCase(destinationAddress, { keypair, url });
+    }
+  });
+
+program.parse(process.argv);
--- corner-cases/scripts/utils.ts
+++ corner-cases/scripts/utils.ts
@@ -1,8 +1,13 @@
-import { Metaplex } from "@metaplex-foundation/js";
+import {
+  bundlrStorage,
+  keypairIdentity,
+  Metaplex,
+} from "@metaplex-foundation/js";
 import type {
   CreateNftInput,
   UploadMetadataInput,
 } from "@metaplex-foundation/js";
+import { Connection } from "@solana/web3.js";
 
 export const createNft = async (
   mx: Metaplex,
@@ -30,3 +35,18 @@ export const createCollectionNft = (
   mx: Metaplex,
   input: Partial<CreateNftInput & { json: UploadMetadataInput }> = {}
 ) => createNft(mx, { ...input, isCollection: true });
+
+export const initialize = ({ keypair, url }) => {
+  const connection = new Connection(url, "finalized");
+  const storage = url.includes("devnet")
+    ? bundlrStorage({
+        address: "https://devnet.bundlr.network",
+      })
+    : bundlrStorage();
+
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(keypair))
+    .use(storage);
+
+  return { metaplex };
+};
